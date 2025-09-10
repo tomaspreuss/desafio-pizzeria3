8ac1a8a41542f9d109e628647f015f3a59bc9c4f
tomaspreuss <tomaspreuss@tomaspreusss-MacBook-Air.local>
2025-09-09 22:42:20 -0300

    Inicio Hito 3 desde base Hito 2

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import RegisterPage from "./RegisterPage";
+
+// function App() {
+//   return (
+//     <>
+//       <RegisterPage />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import LoginPage from "./LoginPage";
+// import RegisterPage from "./RegisterPage";
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+}
+
+export default App;
